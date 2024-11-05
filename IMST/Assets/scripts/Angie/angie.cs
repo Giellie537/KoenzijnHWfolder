2306f5520be47e2d7254226680a6e5acfce7f9fb
Basil-iskos <louise.vanbeek@live.nl>
2024-11-05 15:28:24 +0100

    quest bij angie goed

---
+++ IMST/Assets/scripts/Angie/angie.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class angie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuestsuestSystem.instance.addquest();
+        QuestsuestSystem.instance.addquest();
+    }
+
+
+}
