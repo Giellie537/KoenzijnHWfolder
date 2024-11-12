87275b69ade54728b4cb3533d557316741590164
Basil-iskos <louise.vanbeek@live.nl>
2024-11-12 12:02:50 +0100

    outsidesunday

--- IMST/Assets/scripts/Outside/outsidesunday.cs
+++ IMST/Assets/scripts/Outside/outsidesunday.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class outsidesunday : MonoBehaviour
 {
+
+    public GameObject DialogueMan;
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        DialogueMan.SetActive(true);
+        dialogueText.text = "Koen:\nAllright, time to go to the market.";
+        StartCoroutine(Textweg());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     IEnumerator Textweg() {
+        yield return new WaitForSeconds(3);
+        DialogueMan.SetActive(false);
+     }
+
 }
