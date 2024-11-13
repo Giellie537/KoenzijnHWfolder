23b5a1c4d98f48568b9f6e5920936cfa77a8cd18
Basil-iskos <louise.vanbeek@live.nl>
2024-11-13 13:33:51 +0100

    dialogue supermarkt werkt weer

---
+++ IMST/Assets/scripts/Supermarkt/Startauto.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class startauto : MonoBehaviour
+{
+
+    public GameObject DialogueMan;
+	public TextMeshProUGUI dialogueText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogueMan.SetActive(true);
+        dialogueText.text = "Sarah(1/11):\nThanks Koen!";
+        StartCoroutine(Textweg());
+    }
+
+     IEnumerator Textweg() {
+        yield return new WaitForSeconds(3);
+        DialogueMan.SetActive(false);
+     }
+
+}
